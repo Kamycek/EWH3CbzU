f4d0cea3be00584737150c1ab468a6ef656d16ff
Kamycek <k.mazur9@wp.pl>
2018-05-15 18:32:45 +0200

    wtf?

--- odpowiedzi.js
+++ odpowiedzi.js
@@ -3,15 +3,12 @@ let odpowiedzi = document.querySelectorAll('.test p');
 let tresc = "arr = [";
 const reg =/\s/mg;
     for (var i = 0; i < pytania.length; i++) {
-<<<<<<< HEAD
 		pyt = pytania[i].innerHTML;
 		pyt = pyt.replace(/<([^>]+>)/ig,"");
 		pyt = pyt.replace(reg,"");
 		odp = odpowiedzi[i].innerHTML;
         tresc += '[\''+pyt+'\',\''+odp+'\'],';
-=======
         tresc += '[\''+pytania[i].innerHTML+'\',\''+odpowiedzi[i].innerHTML+'\'],';
->>>>>>> parent of 4180f86... Poprawiony skrypt
     }
 tresc +="]";
 tresc = tresc.replace(reg,"").toLowerCase();
