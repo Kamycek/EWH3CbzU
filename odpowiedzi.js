4180f8689c4f2bc1aa76f413717f9a7188bf1689
Kamycek <k.mazur9@wp.pl>
2018-05-15 14:50:53 +0200

    Poprawiony skrypt

--- odpowiedzi.js
+++ odpowiedzi.js
@@ -1,16 +1,17 @@
 let pytania = document.querySelectorAll('.test h4');
 let odpowiedzi = document.querySelectorAll('.test p');
 let tresc = "arr = [";
-const reg =/\s/mg;
+let pyt;
+let odp;
+const reg =/\W/mgi;
     for (var i = 0; i < pytania.length; i++) {
-        tresc += '[\''+pytania[i].innerHTML+'\',\''+odpowiedzi[i].innerHTML+'\'],';
+		pyt = pytania[i].innerHTML;
+		pyt = pyt.replace(/<([^>]+>)/ig,"");
+		pyt = pyt.replace(reg,"");
+		odp = odpowiedzi[i].innerHTML;
+        tresc += '[\''+pyt+'\',\''+odp+'\'],';
     }
 tresc +="]";
-tresc = tresc.replace(reg,"").toLowerCase();
-tresc = tresc.replace(/<br>/mg,"");
-tresc = tresc.replace(/&gt/mg,"");
-tresc = tresc.replace(/&lt/mg,"");
-tresc = tresc.replace(/;/mg,"");
-tresc = tresc.replace(/„/mg,"");
-tresc = tresc.replace(/”/mg,"");
+tresc = tresc.toLowerCase();
+
 tresc
